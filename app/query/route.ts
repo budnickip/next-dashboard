f7d33e045fa01fc31035924fdb172a258b572755
patryk.budnicki <patryk.budnicki@siili.com>
2024-12-27 15:30:13 +0100

    feat: add prettier and eslint

--- app/query/route.ts
+++ app/query/route.ts
@@ -15,9 +15,8 @@
 
 export async function GET() {
   return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
+    message: 'Uncomment this file and remove this line. You can delete this file when you are finished.',
+  })
   // try {
   // 	return Response.json(await listInvoices());
   // } catch (error) {
