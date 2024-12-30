2a29ca98660ee58f65275394fc4ddb41b93ab201
patryk.budnicki <patryk.budnicki@siili.com>
2024-12-30 12:08:04 +0100

    feat: disable cache in invoices

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,6 +5,9 @@ import { lusitana } from '@/app/ui/fonts'
 import { Suspense } from 'react'
 import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons'
 
+// Note: inaczej dane ktore pobieram z bazy danych sa pobierane tylko raz, podczas deployu apki - potestuj to jeszcze
+export const dynamic = 'force-dynamic'
+
 //Page is an async component. This allows you to use await to fetch data.
 export default async function Page() {
   // The data requests are unintentionally blocking each other, creating a request waterfall.
